b7a08f77b14fadea43a1890c1beb807f58992b75
nuguri03 <sym2596@naver.com>
2026-03-01 07:08:42 +0000

    feat: boot

---
+++ boot.asm
@@ -0,0 +1,24 @@
+[ORG 0x7c00]
+[BITS 16]
+
+
+start:
+    mov si, msg
+    call print
+    jmp $
+
+print:
+    mov ah, 0x0e
+.loop:
+    lodsb
+    cmp al, 0
+    je .end
+    int 0x10
+    jmp .loop
+.end:
+    ret
+    
+msg db 'Hello BootLoader!', 0
+
+times 510-($-$$) db 0
+dw 0xAA55
