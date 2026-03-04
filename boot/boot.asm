294552a505c47f8d0ff3f69209d134480e3d5c8a
yumin <sym2596@knu.ac.kr>
2026-03-04 22:00:45 +0900

    fix: change msg array to pointer

--- boot/boot.asm
+++ boot/boot.asm
@@ -6,12 +6,12 @@ start:
     mov es, ax
     mov bx, 0x0000
 
-    mov ah, 0x02    
-    mov al, 8
+    mov ah, 0x02
+    mov al, 9           ; 9개의 섹터(9 * 512B = 4,608KB)
     mov ch, 0
-    mov cl, 2
-    mov dh, 0
-    mov dl, 0x80
+    mov cl, 2           ; 2번째 섹터부터
+    mov dh, 0           ; 0번째 헤드부터
+    mov dl, 0x80        ; 0x80(HDD)
 
     int 0x13
     jc disk_error
