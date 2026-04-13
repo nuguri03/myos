7b86591cc713638338084f1a37622fe2284e7825
yumin <sym2596@knu.ac.kr>
2026-04-13 17:48:00 +0900

    feat: add boolean, size type and NULL

--- include/types.h
+++ include/types.h
@@ -1,7 +1,6 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-// TODO: int가 32비트임을 보장 할 수 없다. 따라서 #include <stdint.h>를 사용하자.
 typedef unsigned char       u8;
 typedef unsigned short      u16;
 typedef unsigned int        u32;
@@ -10,6 +9,12 @@ typedef signed char         i8;
 typedef signed short        i16;
 typedef signed int          i32;
 
+typedef u8 bool;
+
+typedef u32 size_t;
+typedef i32 ssize_t;
+
+#define NULL ((void*)0)
 #define true 1
 #define false 0
 
